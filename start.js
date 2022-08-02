ace8958da92fc97cf91a6a8d5bbd85a65879317b
jasonhe54 <68795646+jasonhe54@users.noreply.github.com>
2022-08-02 17:40:19 -0400

    fixed regex

--- start.js
+++ start.js
@@ -322,7 +322,7 @@ app.action("TaskDone_ActionID", async ({ ack, client, body }) => {
   for (i=0; i<body.message.blocks.length; i++) {
     if (JSONChannelTS_BlockID == body.message.blocks[i].block_id) {
       var text = body.message.blocks[i].elements[0].text;
-      timestamp = text.match(/`[1-9].*\.[1-9].*`/)[0].split("'")[1];
+      timestamp = text.match(/`[0-9].*\.[0-9].*`/)[0].split("`")[1];
       console.log(timestamp)
     } else {
       continue;
