decd5011ee159a4cd53286bff358ed0380124c19
jasonhe54 <68795646+jasonhe54@users.noreply.github.com>
2022-08-10 10:33:39 -0400

    test?

--- start.js
+++ start.js
@@ -832,7 +832,7 @@ ${paymentDueByDate}
                 "text": "*Current Request Status:*\\nApproved by <@${approverUserID}> at ${time}"
             }
           }`;
-          newUpdatedBlocks.push(newStatusBlock);
+          newUpdatedBlocks.push(JSON.stringify(newStatusBlock));
         } else if (decision == "denied") {
           var newStatusBlock = `{
             "type": "section",
