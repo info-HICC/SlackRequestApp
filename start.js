f9c679fa076ea1635c9e0a3d46ac974557e5b4f4
jasonhe54 <68795646+jasonhe54@users.noreply.github.com>
2022-08-11 23:25:48 -0400

    forgot it's app.client, not app.

--- start.js
+++ start.js
@@ -632,7 +632,7 @@ app.action("selectingRadioButtons_ActionID", async ({ ack, body, client }) => {
   if (buttonPressed == "Cash") {
     var newView = modalViews.requestApp_CreateRequestModalView_test_cash;
     //call slack API to update the view
-    app.views.update({
+    app.client.views.update({
       view: newView,
       view_id: viewID
     })
