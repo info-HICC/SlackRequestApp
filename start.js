2747faab78f2c5030e66007dfb85b103a13f2bff
jasonhe54 <68795646+jasonhe54@users.noreply.github.com>
2022-09-17 11:25:50 -0400

    update to have the original text be sent along when updating the metadata info

--- start.js
+++ start.js
@@ -846,6 +846,7 @@ app.action("approve_approvers_ApproveDeny_BTN_ActionID", async ({ ack, body, cli
     var metadataRequestCost = parseFloat(messageMetadata.messages[0].metadata.event_payload.cost).toFixed(2);//basically turn it from a string to an integer.
     var metadata = messageMetadata.messages[0].metadata; //this is already in JSON (aka an object)
     metadata.event_payload.numberOfApprovals = newMetadataApprovalCount;
+    var originalMessageText = messageMetadata.messages[0].text;
     if (metadataRequestCost >= 10000) { //this checks if the request is over or equal to $10,000 
       //this part can be fixed later to use the modulus (%) operator.
       if (metadataApprovalCount == 0) {
@@ -853,6 +854,7 @@ app.action("approve_approvers_ApproveDeny_BTN_ActionID", async ({ ack, body, cli
         //this checks the count before it adds 1 to it. 
         await client.chat.update({
           channel: channelWithMessageWithBlocks,
+          text: originalMessageText,
           ts: messageBlocksTS,
           metadata: metadata
         });
@@ -862,6 +864,7 @@ app.action("approve_approvers_ApproveDeny_BTN_ActionID", async ({ ack, body, cli
         //this checks the count before it adds 1 to it. 
         await client.chat.update({
           channel: channelWithMessageWithBlocks,
+          text: originalMessageText,
           ts: messageBlocksTS,
           metadata: {
             "event_type": "requestApprovedAction", 
