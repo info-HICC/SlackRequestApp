97b4efac6e3ec6dc2841d457d159335f8395de15
jasonhe54 <68795646+jasonhe54@users.noreply.github.com>
2022-09-21 17:53:53 -0400

    logs

--- start.js
+++ start.js
@@ -979,12 +979,14 @@ app.action("approve_approvers_ApproveDeny_BTN_ActionID", async ({ ack, body, cli
           });
           //if test mode don't send to accountants channel
           if (testStatusFile.test == false) {
+            console.log("is test false required 2 approvers")
             await client.chat.postMessage({
               channel: accountantsChannelID,
               text: "Request has been approved by 2 people. Please review the request and make the payment if necessary. It is my understanding that the accountants already know what will happen from this point on.",
               blocks: blocksForAccountants
             });
           } else if (testStatusFile.test == true) {
+            console.log("is test true required 2 approvers")
             await client.chat.postMessage({
               channel: devchannel,
               text: "Request has been approved by 2 people. Please review the request and make the payment if necessary. It is my understanding that the accountants already know what will happen from this point on.",
@@ -1040,12 +1042,14 @@ app.action("approve_approvers_ApproveDeny_BTN_ActionID", async ({ ack, body, cli
         }
       });
       if (testStatusFile.test == false) {
+        console.log("is test false");
         await client.chat.postMessage({
           channel: accountantsChannelID,
           text: "Request has been approved by 2 people. Please review the request and make the payment if necessary. It is my understanding that the accountants already know what will happen from this point on.",
           blocks: blocksForAccountants
         });
       } else if (testStatusFile.test == true) {
+        console.log("is test true")
         await client.chat.postMessage({
           channel: devchannel,
           text: "Request has been approved by 2 people. Please review the request and make the payment if necessary. It is my understanding that the accountants already know what will happen from this point on.",
